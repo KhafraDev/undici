6e4f5b297365e37b9d84007424ee226bc779e8bc
Khafra <maitken033380023@gmail.com>
2023-10-25 14:19:52 -0400

    skip bundle test on node 16
    skip bundle test on node 16

--- test/fetch/bundle.js
+++ test/fetch/bundle.js
@@ -1,6 +1,13 @@
 'use strict'
 
-const { test } = require('tap')
+const { test, skip } = require('tap')
+const { nodeMajor } = require('../../lib/core/util')
+
+if (nodeMajor === 16) {
+  skip('esbuild uses static blocks with --keep-names which node 16.8 does not have')
+  process.exit()
+}
+
 const { Response, Request, FormData, Headers } = require('../../undici-fetch')
 
 test('bundle sets constructor.name and .name properly', (t) => {
@@ -16,5 +23,19 @@ test('bundle sets constructor.name and .name properly', (t) => {
   t.equal(new FormData().constructor.name, 'FormData')
   t.equal(FormData.name, 'FormData')
 
+  t.end()
+})
+
+test('regression test for https://github.com/nodejs/node/issues/50263', (t) => {
+  const request = new Request('https://a', {
+    headers: {
+      test: 'abc'
+    },
+    method: 'POST'
+  })
+
+  const request1 = new Request(request, { body: 'does not matter' })
+
+  t.equal(request1.headers.get('test'), 'abc')
   t.end()
 })
